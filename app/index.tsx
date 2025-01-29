4bd1364097124db7add3bd1350026b81015bfa63
Shay Reichert <shay.reichert@outlook.fr>
2025-01-29 22:58:37 +0100

    Design system

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ThemedText from "./components/ThemedText";
+import useThemeColors from "@/hooks/useThemeColors";
+import Card from "./components/Card";
 
 export default function Index() {
+  const colors = useThemeColors();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={[styles.container, , { backgroundColor: colors.tint }]}>
+      <Card>
+        <ThemedText variant="headline" color="grayDark">
+          Pokédex
+        </ThemedText>
+      </Card>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
